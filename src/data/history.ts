03dcef8407168ad43f9d878dafd64be71b7442dd
timgoodman047 <tim.goodman047@gmail.com>
2026-09-25 12:43:20 -0500

    Clear history data
    Reset history array to an empty state.

--- src/data/history.ts
+++ src/data/history.ts
@@ -1,13 +1,1 @@
-export const history = [
-{
-year: 2025,
-champion: "Short Board Champ",
-owner: "Chris"
-},
- 
-{
-year: 2024,
-champion: "Link?",
-owner: "Jeff"
-}
-];
+export const history = [];
