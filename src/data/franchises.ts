1b495d05d8ebb7e36b5b6c96948f7d74659664e8
timgoodman047 <tim.goodman047@gmail.com>
2026-09-25 13:04:03 -0500

    Update franchises.ts

--- src/data/franchises.ts
+++ src/data/franchises.ts
@@ -3,7 +3,7 @@ export const franchises = [
 {
 slug: "danny",
 owner: "Danny",
-record: "145-104-4",
+overallRecord: "145-104-4",
 winningPct: 0.581,
 championships: 4,
 playoffTrips: 15,
@@ -16,7 +16,7 @@ notes:
 {
 slug: "tim",
 owner: "Tim",
-record: "145-106-2",
+overallRecord: "145-106-2",
 winningPct: 0.577,
 championships: 3,
 playoffTrips: 15,
@@ -29,7 +29,7 @@ notes:
 {
 slug: "brian",
 owner: "Brian",
-record: "131-121-1",
+overallRecord: "131-121-1",
 winningPct: 0.520,
 championships: 2,
 playoffTrips: 12,
@@ -42,7 +42,7 @@ notes:
 {
 slug: "nick",
 owner: "Nick",
-record: "55-53-1",
+overallRecord: "55-53-1",
 winningPct: 0.509,
 championships: 2,
 playoffTrips: 5,
@@ -55,59 +55,59 @@ notes:
 {
 slug: "chris",
 owner: "Chris",
-record: "103-133-3",
+overallRecord: "103-133-3",
 winningPct: 0.437,
 championships: 2,
 playoffTrips: 8,
 highestScore: 198.72,
 highestPlayoffScore: 192.88,
 notes:
-"2025 champion."
+"2025 Champion."
 },
  
 {
 slug: "jason",
 owner: "Jason",
-record: "102-96-2",
+overallRecord: "102-96-2",
 winningPct: 0.515,
 championships: 1,
 playoffTrips: 9,
 highestScore: 179.18,
 highestPlayoffScore: 179.18,
 notes:
-"2015 champion."
+"2015 Champion."
 },
  
 {
 slug: "jeff",
 owner: "Jeff",
-record: "36-34-0",
+overallRecord: "36-34-0",
 winningPct: 0.514,
 championships: 1,
 playoffTrips: 4,
 highestScore: 191.42,
 highestPlayoffScore: 191.42,
 notes:
-"2024 champion."
+"2024 Champion."
 },
  
 {
 slug: "tom",
 owner: "Tom",
-record: "107-131-1",
+overallRecord: "107-131-1",
 winningPct: 0.450,
 championships: 0,
 playoffTrips: 9,
 highestScore: 178.48,
 highestPlayoffScore: 177.24,
 notes:
-"Original member with multiple title-game appearances."
+"Original member with multiple playoff appearances."
 },
  
 {
 slug: "spencer",
 owner: "Spencer",
-record: "101-137-1",
+overallRecord: "101-137-1",
 winningPct: 0.425,
 championships: 0,
 playoffTrips: 6,
@@ -115,6 +115,20 @@ highestScore: 191.84,
 highestPlayoffScore: 185.08,
 notes:
 "2023 runner-up."
+},
+ 
+{
+slug: "matt",
+owner: "Matt",
+overallRecord: "113-140-0",
+winningPct: 0.447,
+championships: 0,
+playoffTrips: 10,
+highestScore: 198.72,
+highestPlayoffScore: 172.58,
+notes:
+"Longest winning streak of five games in 2016."
 }
  
 ];
+``
